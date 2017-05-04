11d2f8adf8b3ba2e50b589be3ca79fa5147b56e1
stephenc222 <stephenc211@gmail.com>
2017-05-04 16:31:13 -0500

    player now has keyDown event handlers attached to create movement

--- src/App/index.js
+++ src/App/index.js
@@ -4,21 +4,157 @@ import Maze from './Maze'
 import './index.css';
 
 const MAZE_WIDTH = 20
-const MAZE_Height = 10
+const MAZE_Height = 11
+
+const KEY = {
+  // END: 35,
+  // HOME: 36,
+  LEFT: 37,
+  RIGHT: 39,
+  UP: 38,
+  DOWN: 40,
+  // BACKSPACE: 8,
+  // DELETE: 46,
+  // ENTER: 13
+}
+
+// const CURSOR_KEYS = [
+//   KEY.UP,
+//   KEY.DOWN,
+//   KEY.LEFT,
+//   KEY.RIGHT
+// ]
 class App extends Component {
   constructor (props) {
     super(props)
 
+    this.getTileType = this.getTileType.bind(this)
+    this.onKeyDown = this.onKeyDown.bind(this)
+    this.movePlayerUp = this.movePlayerUp.bind(this)
+    this.movePlayerDown = this.movePlayerDown.bind(this)
+    this.movePlayerLeft = this.movePlayerLeft.bind(this)
+    this.movePlayerRight = this.movePlayerRight.bind(this)
+    this.redraw = this.redraw.bind(this)
+    // this.onKeyPress = this.onKeyPress.bind(this)
+
     this.state = {
       player: {
         x: 1,
-        y: 1,
+        y: 5,
         invincible: false
-      }
+      },
+      maze: [
+      'xxxxxxxxxxxxxxxxxxxx',    
+      'xO        x       Ox',    
+      'x x xxxxx x  xxx x x',    
+      'x x     x xx     x x',    
+      'x xxx x x  xxx xxx x',    
+      'x     x x      x   x',    
+      'x xxx x x  xxx xxx x',    
+      'x x     xx x     x x',    
+      'x x xxx  x xxxxx x x',    
+      'xO                Ox',    
+      'xxxxxxxxxxxxxxxxxxxx',  
+      ]
     }
   }
 
   componentWillMount () {
+    // const mazeContent = []
+
+    // let x = 0
+    // let y = 0
+
+    // while (mazeContent.length < MAZE_Height) {
+    //   mazeContent[y] = []
+    //   x = 0
+    //   while (mazeContent[y].length < MAZE_WIDTH) {
+    //     mazeContent[y].push({
+    //       'x':x, 
+    //       'y':y, 
+    //       type: this.getTileType(x,y)
+    //     })
+    //     ++x
+    //   }
+    //   ++y
+    // }
+
+    // this.setState({mazeContent})
+    this.redraw()
+  }
+
+  shouldComponentUpdate () {
+    return true
+  }
+
+  componentDidMount () {
+    // console.log(this.game)
+    this.game.focus()
+  }
+
+
+  getTileType (x,y) {
+
+    const maze = this.state.maze.slice()
+    const player = {...this.state.player}
+    const chipToTileId = chip => {    
+      // console.log(JSON.stringify(chip,null,2))
+      if (chip === 'x') {      
+        return 'wall'    
+      } else if (chip === 'O') {      
+        return 'biscuit--big'    
+      } else if (x === player.x && y === player.y){      
+        return 'player'    
+      } else {
+        return 'biscuit'
+      }
+    }
+
+    const updateMaze = []
+
+    for (let row in maze) {
+      updateMaze.push(maze[row].split('').map(chipToTileId))
+    }
+    
+    return updateMaze[y][x]
+  }
+
+  onKeyDown (event) {
+  // onKeyPress (event) {
+    const {
+      // altKey,
+      // ctrlKey,
+      // key,
+      // locale,
+      // location,
+      // metaKey,
+      // repeat,
+      // which,
+      //charCode,
+      keyCode,
+      //shiftkey
+    } = event
+
+    const movePlayerUp = this.movePlayerUp
+    const movePlayerDown = this.movePlayerDown
+    const movePlayerLeft = this.movePlayerLeft
+    const movePlayerRight = this.movePlayerRight
+
+    console.log('keyCode is: ', keyCode)
+
+    if (keyCode === KEY.UP) {
+      movePlayerUp()
+    } else if (keyCode === KEY.DOWN) {
+      movePlayerDown()
+    } else if (keyCode === KEY.LEFT) {
+      movePlayerLeft()
+    } else if (keyCode === KEY.RIGHT) {
+      movePlayerRight()
+    }
+    // this.redraw()      
+  }
+
+  redraw () {
     const mazeContent = []
 
     let x = 0
@@ -31,10 +167,7 @@ class App extends Component {
         mazeContent[y].push({
           'x':x, 
           'y':y, 
-          // player:false, 
           type: this.getTileType(x,y)
-          // wall:(x === 0 || y === 0 || y === 29) ? true: false,
-          // biscuit: !(x === 0 || y === 0 || y === 29) ? true: false
         })
         ++x
       }
@@ -43,51 +176,63 @@ class App extends Component {
 
     this.setState({mazeContent})
   }
-
-
-  getTileType (x,y) {
-    const chipToTileId = chip => {    
-      if (chip === 'x') {      
-        return 'wall'    
-      } else if (chip === 'O') {      
-        return 'biscuit--big'    
-      } else {      
-        return 'biscuit'    
-      }  
-    }
-
-    return [    
-      'xxxxxxxxxxxxxxxxxxxx'.split('').map(chipToTileId),    
-      'xO        x       Ox'.split('').map(chipToTileId),    
-      'x x xxxxx x  xxx x x'.split('').map(chipToTileId),    
-      'x x     x xx     x x'.split('').map(chipToTileId),    
-      'x xxx x x  xxx xxx x'.split('').map(chipToTileId),    
-      'x     x x      x   x'.split('').map(chipToTileId),    
-      'x xxx x x  xxx xxx x'.split('').map(chipToTileId),    
-      'x x     xx x     x x'.split('').map(chipToTileId),    
-      'x x xxx  x xxxxx x x'.split('').map(chipToTileId),    
-      'xO                Ox'.split('').map(chipToTileId),    
-      'xxxxxxxxxxxxxxxxxxxx'.split('').map(chipToTileId),  
-    ][y][x]
+  movePlayerUp () {
+    console.log('move up!')
+    const player = {...this.state.player}
+    player.y--
+    console.log(JSON.stringify(player,null,2))
+    this.setState({player},this.redraw)
   }
 
-  //   if (x === 0 || y === 0 || y === 29 || x === 29) {
-  //     return 'wall'
-  //   } else if (
-  //     (x === 1 && y === 28)
-  //     || (x === 28 && y === 1) 
-  //     || (x === 28 && y === 28)) {
-  //     // || (x === 1  && y === 1)){
-  //     return 'biscuit--big'
-  //   } else if(x === 1 && y === 1) {
-  //     return 'player'
-  //   } else {
-  //     return 'biscuit'
-  //   }
+  movePlayerDown () {
+    console.log('move down!')
+    const player = {...this.state.player}
+    player.y++
+    console.log(JSON.stringify(player,null,2))
+    this.setState({player},this.redraw)
+  }
+
+  movePlayerLeft () {
+    console.log('move left!')
+    const player = {...this.state.player}
+    player.x--
+    console.log(JSON.stringify(player,null,2))
+    this.setState({player},this.redraw)
+  }
+
+  movePlayerRight () {
+    console.log('move right!')
+    const player = {...this.state.player}
+    player.x++
+    console.log(JSON.stringify(player,null,2))
+    this.setState({player},this.redraw)
+  }
+
+  // onKeyPress (event) {
+  //   const {
+  //     // altKey,
+  //     // ctrlKey,
+  //     // key,
+  //     // locale,
+  //     // location,
+  //     // metaKey,
+  //     // repeat,
+  //     // which,
+  //     charCode,
+  //     keyCode,
+  //     shiftKey
+  //   } = event
+
   // }
+
   render() {
     return (
-      <div className="game-container">
+      <div 
+        className="game-container" 
+        tabIndex={0}
+        onKeyDown={this.onKeyDown}
+        // onKeyPress={this.onKeyPress}
+        ref={(element) => {this.game = element}}>
         <Maze
           mazeContent={this.state.mazeContent}
         />
